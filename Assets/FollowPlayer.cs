23ca9597819d44bcfab4e212fd938c8925bf0c2f
DevExcy <devexcy@gmail.com>
2024-05-09 23:39:34 +0100

    New player controller and vehicles

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectGateway
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        private Vector3 offset;
+        private Transform player;
+        
+        // Start is called before the first frame update
+        void Awake()
+        {
+            offset = transform.position;
+            player = GameObject.FindWithTag("Player").transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = player.transform.position += offset;
+        }
+    }
+}
