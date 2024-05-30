7469693732e36f6921032c3afdbe18e022376154
DevExcy <devexcy@gmail.com>
2024-05-30 23:12:20 +0100

    Added doors

---
+++ Assets/DoorObject.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ProjectGateway
+{
+    public class DoorObject : Prop, IInteractable
+    {
+        public bool shut;
+        public bool locked;
+        
+        private PhysicsDoor _door;
+        // Start is called before the first frame update
+        protected new void Awake()
+        {
+            base.Awake();
+            _door = GetComponentInParent<PhysicsDoor>();
+        }
+
+        void Start()
+        { 
+            if (shut || locked)
+            {
+                _door.Shut();
+            }
+        }
+        
+        public new Dictionary<InteractType, string> GetInteractText(InteractContext context)
+        {
+            // Get the base prop interactions
+            var interactList = new Dictionary<InteractType, string>();
+            if (shut)
+            {
+                interactList.Add(InteractType.Use, "Open");
+            }
+            else
+            {
+                interactList.Add(InteractType.Grab, "Grab");
+            }
+            // Return the modified list
+            return interactList;
+        }
+        
+        public new void Interact(InteractType interactType, InteractContext context)
+        {
+            switch (interactType)
+            {
+                case InteractType.Use:
+                    if (locked)
+                    {
+                        FeedbackMessageUIManager.instance.ShowMessage("It's locked");    
+                    }
+                    else
+                    {
+                        shut = false;
+                        _door.Open();
+                    }
+                    break;
+                case InteractType.Grab:
+                    if (!shut && !locked)
+                    {
+                        base.Interact(interactType, context);
+                    }
+                    break;
+            }
+        }
+    }
+}
