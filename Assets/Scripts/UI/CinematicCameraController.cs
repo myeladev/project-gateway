3203e0ea6719d4f017c89432509cb4375084aa44
myeladev <devexcy@gmail.com>
2025-09-21 13:18:10 +0100

    Added cycle to cinematic shots in main menu

---
+++ Assets/Scripts/UI/CinematicCameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cinemachine;
+using UnityEngine;
+
+namespace ProjectGateway.UI
+{
+    public class CinematicCameraController : MonoBehaviour
+    {
+        private List<CinemachineVirtualCamera> cameras = new ();
+        private int index;
+        
+        void Start()
+        {
+            cameras = GetComponentsInChildren<CinemachineVirtualCamera>().ToList();
+            index = Random.Range(0, cameras.Count);
+            foreach (var cam in cameras)
+            {
+                cam.gameObject.SetActive(false);
+            }
+            cameras[index].gameObject.SetActive(true);
+        }
+
+        private void Update()
+        {
+            if (Mathf.Approximately(cameras[index].GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition,
+                    1f))
+            {
+                cameras[index].gameObject.SetActive(false);
+                index++;
+                index %= cameras.Count;
+                cameras[index].gameObject.SetActive(true);
+                Camera.main.transform.position = cameras[index].transform.position;
+            }
+        }
+    }
+}
